902240baafa40432fcbb23fc8873a6b2642031fe
Jonathan Mai <h.jmai2791@gmail.com>
2018-07-22 19:32:06 -0700

    port change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var friends = require('./app/data/friends');
+var PORT = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +9,6 @@ app.use(bodyParser.json());
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000!');
+app.listen(PORT, function () {
+    console.log('Listening on port ' + PORT);
 })
